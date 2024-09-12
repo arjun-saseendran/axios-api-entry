573c843d911e76cc8f97c44b29cfb0ead9589212
arjun-saseendran <arjunsaseendran@icloud.com>
2024-09-12 11:30:18 +0530

    add footer

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -7,12 +7,12 @@ function Cards({ product }) {
     <>
       {product.map((product, index) => (
         <Col xs={6} md={4} lg={3} key={index} className="p-2">
-          <Card>
+          <Card style={{background: '#FFF8E8'}}>
             <Card.Img style={{height: '200px', objectFit: 'contain' , padding: '10px'}} variant="top" src={product.image}/>
             <Card.Body>
               <Card.Title style={{maxHeight: '20px', overflow: 'hidden'}}>{product.title}</Card.Title>
               <Card.Text style= {{maxHeight: '100px', overflow: 'hidden'}}>{product.description}</Card.Text>
-              <Button variant="primary" style={{background: 'red'}}>Add to cart</Button>
+              <Button className="text-white" variant="dark" style={{background: '#603F26'}}>Add to cart</Button>
             </Card.Body>
           </Card>
         </Col>
