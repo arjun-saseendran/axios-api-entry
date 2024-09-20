92aa00e6601fef6689a42758dbc10802886b417a
arjun-saseendran <arjunsaseendran@icloud.com>
2024-09-20 18:53:08 +0530

    add Product.jsx

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../header/Header";
 import { Footer } from "../footer/Footer";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Row } from "react-bootstrap";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+  const { product_id } = useParams();
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products/${product_id}`)
+      .then((response) => setProduct(response.data));
+  }, []);
+
   return (
     <>
       <Header />
       <Row>
         <Card style={{ background: "#FFF8E8" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img
+            className="mx-auto w-25 p-5"
+            variant="top"
+            src={product.image}
+          />
           <Card.Body>
-            <Card.Title>Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>{product.description}</Card.Text>
+            <Card.Text>${product.price}</Card.Text>
+            <Button variant="dark" style={{ background: "#603F26" }}>
+              Buy now{" "}
+            </Button>
           </Card.Body>
         </Card>
       </Row>
